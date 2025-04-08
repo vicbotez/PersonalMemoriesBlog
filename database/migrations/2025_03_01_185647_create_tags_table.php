aca0e379c2280e89ea829544d1101a816b68d268
Victor Botez <victor@italtrade.ro>
2025-04-08 12:38:37 +0300

    15

--- database/migrations/2025_03_01_185647_create_tags_table.php
+++ database/migrations/2025_03_01_185647_create_tags_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
   {
     Schema::create('tags', function (Blueprint $table) {
       $table->id();
-      $table->string('title');
+      $table->string('title',250);
       $table->softDeletes();
       $table->timestamps();
     });
