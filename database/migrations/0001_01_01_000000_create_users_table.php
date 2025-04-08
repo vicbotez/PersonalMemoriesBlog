f96596c615c1dd93fb8c01de0c6d9d8069b5dca8
Victor Botez <victor@italtrade.ro>
2025-04-08 12:08:47 +0300

    05

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -25,12 +25,12 @@ return new class extends Migration
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email',128)->primary();
-            $table->string('token',256);
+            $table->string('token',128);
             $table->timestamp('created_at')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
+            $table->string('id',16)->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
