bfe7f23642ec0cac06fe93ae6d87b6e3749a4080
Victor Botez <victor@italtrade.ro>
2025-04-08 12:31:20 +0300

    11

--- database/migrations/2025_03_01_185640_create_posts_table.php
+++ database/migrations/2025_03_01_185640_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
   {
     Schema::create('posts', function (Blueprint $table) {
       $table->id();
-      $table->string('title');
+      $table->string('title',254);
       $table->text('content');
       $table->text('user_id');
       $table->softDeletes();
