b859a271166005984a2c88a3ae4c339a65b3bbd3
Victor Botez <victor@italtrade.ro>
2025-04-08 10:46:38 +0300

    08-04-2025

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,11 +34,11 @@ class AppServiceProvider extends ServiceProvider
       // share configurations
       $config = (Object) Config::all()->pluck('value', 'key')->toArray();
     }
-    View::share('confBlogName', $config->blog_name);
-    View::share('confBlogTitle', $config->blog_title);
-    View::share('confBlogMetaTitle', $config->blog_meta_title);
-    View::share('confBlogMetaDescription', $config->blog_meta_description);
-    View::share('confBlogFavicon', $config->blog_favicon);
+    View::share('confBlogName', $config->blog_name ?? '');
+    View::share('confBlogTitle', $config->blog_title ?? '');
+    View::share('confBlogMetaTitle', $config->blog_meta_title ?? '');
+    View::share('confBlogMetaDescription', $config->blog_meta_description ?? '');
+    View::share('confBlogFavicon', $config->blog_favicon ?? '');
 
     // share tags
     View::share('tagList', Tag::all());
